53f49ee597509df5ec4a9f5ac67856802bee3ec7
Alouiscious Sims <info@reneesoul.com>
2019-07-13 06:33:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array["Susan", "Renee", "La Roca", "Sims"]
+  Array["Susan", "Renee", "La Roca", "Sims"]
   # four_items["Susan", "Renee", "La Roca", "Sims"]
 end
 
