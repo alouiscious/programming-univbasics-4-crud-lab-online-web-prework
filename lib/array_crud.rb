ece2f4d9b7dd96ffdc5a609a3e65482f4dc14775
Alouiscious Sims <info@reneesoul.com>
2019-07-13 06:38:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  # add_element_to_start_of_array(array, "puppy").unshift
+array.unshift("puppy")
 end
 
 def remove_element_from_end_of_array(array)
