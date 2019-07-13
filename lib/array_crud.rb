b69f3f86c906be0d21bed8f5224f2f0c35f3d27a
Alouiscious Sims <info@reneesoul.com>
2019-07-13 06:16:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-push
+  array[]
+  add_element_to_end_of_array(array.push, "monkey")
 end
 
 def add_element_to_start_of_array(array, element)
